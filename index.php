4d8016985e1ca71060b69219a4f4aaa7218e06f1
PocketKiller <pocketkiller@imagicalmine.me>
2016-01-29 22:41:10 +0300

    Testing :D

--- index.php
+++ index.php
@@ -14,18 +14,18 @@
       <h1 class="project-name">Mcrafterss</h1>
       <h2 class="project-tagline">Pocketmine Plugin Developers Team</h2>
       <a href="http://mcrafterss.github.io/Op-Manger/" class="btn">Op-Manger</a>
-      <a href="http://mcrafterss.github.io/MTeamPvP" class="btn">MTeampvp</a>
+      <a href="http://mcrafterss.github.io/MTeamPvP" class="btn">MTeamPvP</a>
       <a href="http://mcrafterss.github.io/MSpleef" class="btn">MSpleef</a>
       <a href="http://mcrafterss.github.io/MFlyCommand" class="btn">MFly Command</a>
     </section>
 
     <section class="main-content">
       <h3>
-<a id="this-page-is-not-coded-yet" class="anchor" href="#this-page-is-not-coded-yet" aria-hidden="true"><span aria-hidden="true" class="octicon octicon-link"></span></a>This Page Is Not Coded Yet</h3>
+<a id="this-page-is-not-coded-yet" class="license" href="#this-page-is-not-coded-yet" aria-hidden="true"><span aria-hidden="true" class="octicon octicon-link">Test : <?php echo mt_rand(2, 9); ?> !</span></a></h3>
 
       <footer class="site-footer">
 
-        <span class="site-footer-credits">MCrafters Github Website</span>
+        <span class="site-footer-credits">MCrafters Github Website*</span>
       </footer>
 
     </section>
